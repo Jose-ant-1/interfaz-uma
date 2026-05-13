a6c4888918fcc284443666f84ccc9d2e377984d0
Jose-ant-1 <jgonsol1510@g.educaand.es>
2026-05-13 14:24:49 +0200

    añadida opción a descargar apk

--- src/app/environment.ts
+++ src/app/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:8080/api' // La base de todas las llamadas
+  apiUrl: 'http://localhost:8081/api' // La base de todas las llamadas
 };
